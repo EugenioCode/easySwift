9184d1932f4f3fc94586c156e9714465efecbace
eugeniocode <eugeniocode@yeah.net>
2022-08-01 19:29:41 +0800

    Initial Commit

---
+++ easySwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  easySwift
+//
+//  Created by jianjun.yang on 2022/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ easySwift/easySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  easySwiftApp.swift
+//  easySwift
+//
+//  Created by jianjun.yang on 2022/8/1.
+//
+
+import SwiftUI
+
+@main
+struct easySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
